6363e0f98e5ab52e431cc611574165dfe1687fb6
George Jose <george.jose.a@gmail.com>
2017-08-26 12:08:13 -0400

    Made headless by default. Make code slightly cleaner

--- index.js
+++ index.js
@@ -1,56 +1,11 @@
 const p = require("puppeteer");
 
-let USERNAME = "";
-let PASSWORD = "";
-let URL = "";
-
-const _login = async (page, username = "admin", password = "nimda") => {
-  await page.focus("input[name=j_password]");
-  page.type(password);
-  await page.click("button[type=submit]");
-};
-
-const _openFlexibleSearch = async page => {
-  await page.waitFor("#console");
-  await page.click("#console");
-  await page.click("[data-menuitem=flexiblesearch]");
-};
-
-const _query = async (page, query) => {
-  await page.waitForNavigation({ waitUntil: "load" });
-  await page.click(".CodeMirror-scroll");
-  page.type(query);
-  // page.click(".buttonSubmit", { button: "left" });
-  await page.waitFor(1000); // Not sure why this is required
-  const submitButton = await page.$(".buttonSubmit");
-  await submitButton.click({ clickCount: 10 });
-  await page.waitFor("table");
-  const rows = await page.evaluate(() => {
-    const _rows = Array.from(document.querySelectorAll("tr"));
-    const keys = Array.from(_rows[0].querySelectorAll("th")).map(
-      item => item.textContent
-    );
-
-    const _rowsWithCols = _rows.filter((_, index) => index !== 0).map(row => {
-      return Array.from(row.querySelectorAll("td")).map(
-        item => item.textContent
-      );
-    });
-
-    return _rowsWithCols.map(_row => {
-      let obj = {};
-      keys.forEach((key, index) => {
-        obj[key] = _row[index];
-      });
-      return obj;
-    });
-  });
-  return rows;
-};
+let USERNAME, PASSWORD, URL, HEADLESS;
 
 const query = async querytxt => {
+  // Launch browser, go to page, set viewport
   const browser = await p.launch({
-    headless: false,
+    headless: HEADLESS,
     ignoreHTTPSErrors: true
   });
   const page = await browser.newPage();
@@ -58,17 +13,63 @@ const query = async querytxt => {
   await page.goto(URL, {
     waituntil: "networkidle"
   });
-  await _login(page);
-  await _openFlexibleSearch(page);
-  const rows = await _query(page, querytxt);
+
+  // Type in credentials
+  await (async () => {
+    await page.focus("input[name=j_password]");
+    page.type(PASSWORD);
+    await page.click("button[type=submit]");
+  })();
+
+  // Go to flexible search console
+  await (async () => {
+    await page.waitFor("#console");
+    await page.click("#console");
+    await page.click("[data-menuitem=flexiblesearch]");
+  })();
+
+  // Type flexiblesearch query, hit submit, go to results, read table, return rows
+  const rows = await (async () => {
+    await page.waitForNavigation({ waitUntil: "load" });
+    await page.click(".CodeMirror-scroll");
+    page.type(querytxt);
+    await page.waitFor(1000);
+    const submitButton = await page.$(".buttonSubmit");
+    await submitButton.click({ clickCount: 10 });
+    await page.waitFor("table");
+
+    // Run this in browser context, return rows to node context
+    const rows = await page.evaluate(() => {
+      const _rows = Array.from(document.querySelectorAll("tr"));
+      const keys = Array.from(_rows[0].querySelectorAll("th")).map(
+        item => item.textContent
+      );
+
+      const _rowsWithCols = _rows.filter((_, index) => index !== 0).map(row => {
+        return Array.from(row.querySelectorAll("td")).map(
+          item => item.textContent
+        );
+      });
+
+      return _rowsWithCols.map(_row => {
+        let obj = {};
+        keys.forEach((key, index) => {
+          obj[key] = _row[index];
+        });
+        return obj;
+      });
+    });
+    return rows;
+  })();
   return rows;
 };
 
 const initialize = (options = {}) => {
-  const { username, password, url } = options;
+  const { username, password, url, showBrowser } = options;
   USERNAME = username || "admin";
   PASSWORD = password || "nimda";
   URL = url || "https://localhost:9002/admin";
+  HEADLESS = !showBrowser;
 };
 
 exports.query = query;
